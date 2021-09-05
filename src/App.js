6b8c1a79daf522177d236b1f7049406ed68aa4cc
tonyarice <tonyacrice@gmail.com>
2021-09-05 02:15:44 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./images/bit-tcr.jpg"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://react.js.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </header>
+      Hello
+
+    </div>
+  );
+}
+
