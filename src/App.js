672d1b9f89d5fe1758029c4ffd4bf81e13e573f7
tonyarice <tonyacrice@gmail.com>
2021-09-07 20:41:46 -0400

    Added text to header

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ export default function App() {
       <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
-        
+        <h2 className="App-name">What Word Do You Want To Find?</h2>
       </header>
       <main>
+        <br />
         <Dictionary />
           </main>
       <footer className="App-footer"><a href="https://github.com/tonyarice/dictionary-project">Open-source code</a> by Tonya Rice</footer>
