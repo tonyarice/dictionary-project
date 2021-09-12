abec3620d223357a3a4ad27e8d13b363d43b7d1e
tonyarice <tonyacrice@gmail.com>
2021-09-12 10:47:41 -0400

    Added Photos component
    Obtained api from Pexels

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,35 +2,44 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
-
+import Photos from "./Photos";
 
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [results, setResults] = useState(null);
+    let [photos, setPhotos] = useState(null);
 
-    function handleResponse(response) {
+    function handleDictionaryResponse(response) {
         console.log(response.data[0]);
         // console.log(response.data[0].meanings[0].definitions[0].definition);
         setResults(response.data[0])
     }
 
+    function handlePexelsResponse(response) {
+        console.log(response.data);
+        setPhotos(response.data.photos);
+       
+    }
+
 function search(event) {
     event.preventDefault();
     // alert(`Searching for definition of ${keyword}.`);
-
     //documentation" http://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey = "563492ad6f91700001000001304f6c49c87d460e8dffdb8cee65580f";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
 }
 
-function handleKeywordChange(event) {
-    
+function handleKeywordChange(event) { 
     //need to set state to keep this to use later
     setKeyword(event.target.value);
 }
-
     return (
         <div className="Dictionary">
             <section>
@@ -41,7 +50,9 @@ function handleKeywordChange(event) {
                     suggested words: sunset, flower, truck...
                 </div>
                 </section> 
-                <div><Results results={results} />
+                <div>
+                    <Results results={results} />
+                    <Photos photos={photos} />
                </div>
                 </div>
     );
